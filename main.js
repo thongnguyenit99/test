dcaf975ecc41bbdf218ebc6fc90165a88ac169ea
thongnguyen <you@example.com>
2020-06-06 22:28:20 +0700

    done file main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("34fndk");
+function tong(x,y) {
+    return x + y;
+}
+x = 8, y = 9;
+const s = tong(x, y);
+// s=tong(8,9);
+console.log(`${x} + ${y} = ${s}`);
+
+
+ // arrow function
+//(x, y) => x + y
+//hàm trong hàm
+console.log(`${x}`);
